2c30540ea2d70379e3c9b3477f2c52e0712a161c
JoaoPedroCanavez <jsantossilvano@gmail.com>
2025-09-01 14:08:43 -0300

    serialização de episodeos

--- watchmaniac/src/main/java/com/watchmaniac/watchmaniac/WatchmaniacApplication.java
+++ watchmaniac/src/main/java/com/watchmaniac/watchmaniac/WatchmaniacApplication.java
@@ -2,6 +2,7 @@ package com.watchmaniac.watchmaniac;
 
 import com.watchmaniac.service.ConsumoApi;
 import com.watchmaniac.service.ConverteDados;
+import model.DadosEpisodeo;
 import model.DadosSerie;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,10 +18,13 @@ public class WatchmaniacApplication implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
         var consumoapi = new ConsumoApi();
-        var json = consumoapi.obterDados("");   
+        var json = consumoapi.obterDados("https://omdbapi.com/?t=breaking+bad&apikey=");   
         System.out.println(json);
         var conversor = new ConverteDados();
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
+        var json2 = consumoapi.obterDados("https://omdbapi.com/?t=breaking+bad&season=1&episode=2&apikey=");
+        DadosEpisodeo dadosepisodeo = conversor.obterDados(json2, DadosEpisodeo.class);
+        System.out.println(dadosepisodeo);
     }
 }
---
+++ watchmaniac/src/main/java/model/DadosEpisodeo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Record.java to edit this template
+ */
+package model;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+/**
+ *
+ * @author jsant
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosEpisodeo(@JsonAlias("Title")String titulo,
+                            @JsonAlias("Episode")Integer numero,
+                            @JsonAlias("imdbRating")String avaliacao,
+                            @JsonAlias("Released")String dataLancamento) {
+    
+}
